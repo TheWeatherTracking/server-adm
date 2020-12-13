71d574c9fb11e8eaf72b4868dfad8ec7cd751777
ivan7ce <frostow12h@gmail.com>
2020-12-13 17:06:38 +0300

    Add run app script

---
+++ setup/run_app.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+HOME="/home/developer"
+LOG="$HOME/app_`date --iso-8601=seconds`.log"
+
+if not ls "$HOME/app.jar" then
+  echo 'No app.jar was found'
+  exit 1
+
+nohup java -jar "$HOME/app.jar" > "$LOG" 2>&1 &
+
+exit 0
