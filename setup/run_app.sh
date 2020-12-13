b805c71ec606e1ccf0850e05367118534694ebb0
ivan7ce <frostow12h@gmail.com>
2020-12-13 17:11:43 +0300

    Fix run_app.sh

--- setup/run_app.sh
+++ setup/run_app.sh
@@ -3,11 +3,6 @@
 HOME="/home/developer"
 LOG="$HOME/app_`date --iso-8601=seconds`.log"
 
-if not ls "$HOME/app.jar" then
-  echo 'No app.jar was found'
-  exit 1
-fi
-
 nohup java -jar "$HOME/app.jar" > "$LOG" 2>&1 &
 
 exit 0
